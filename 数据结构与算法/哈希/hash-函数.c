cc89d33d86f91e8f59a9b90abb2bdfd318d845a7
peimin <peimin@outlook.com>
2014-04-14 21:32:21 +0800

    haxi

---
+++ 数据结构与算法/哈希/hash-函数.c
@@ -0,0 +1,38 @@
+
+// =======================================================================================================
+// leveldb 中的哈希函数
+inline uint32_t Hash(const char* data, size_t n, uint32_t seed) 
+{
+  // Similar to murmur hash
+  const uint32_t m = 0xc6a4a793;
+  const uint32_t r = 24;
+  const char* limit = data + n;
+  uint32_t h = seed ^ (n * m);
+
+  // Pick up four bytes at a time
+  while (data + 4 <= limit) {
+    uint32_t w = DecodeFixed32(data);
+    data += 4;
+    h += w;
+    h *= m;
+    h ^= (h >> 16);
+  }
+
+  // Pick up remaining bytes
+  switch (limit - data) {
+    case 3:
+      h += data[2] << 16;
+      FALLTHROUGH_INTENDED;// do { } while (0); 不太清楚这里为什么加这个
+    case 2:
+      h += data[1] << 8;
+      FALLTHROUGH_INTENDED;
+    case 1:
+      h += data[0];
+      h *= m;
+      h ^= (h >> r);
+      break;
+  }
+  return h;
+}
+
+// ================================================================================================
