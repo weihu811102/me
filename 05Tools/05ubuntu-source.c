61eac9c40f117f1fe07dd0d8f78410818eb168a8
peimin <peimin@outlook.com>
2014-08-28 10:48:26 +0800

    ubuntu source

---
+++ 05Tools/05ubuntu-source.c
@@ -0,0 +1,14 @@
+sudo vi /etc/apt/sources.list
+sudo apt-get update 
+
+#网易 Ubuntu 11.10 源（速度很快）  
+deb http://mirrors.163.com/ubuntu/ precise main universe restricted multiverse  
+deb-src http://mirrors.163.com/ubuntu/ precise main universe restricted multiverse  
+deb http://mirrors.163.com/ubuntu/ precise-security universe main multiverse restricted  
+deb-src http://mirrors.163.com/ubuntu/ precise-security universe main multiverse restricted  
+deb http://mirrors.163.com/ubuntu/ precise-updates universe main multiverse restricted  
+deb http://mirrors.163.com/ubuntu/ precise-proposed universe main multiverse restricted  
+deb-src http://mirrors.163.com/ubuntu/ precise-proposed universe main multiverse restricted  
+deb http://mirrors.163.com/ubuntu/ precise-backports universe main multiverse restricted  
+deb-src http://mirrors.163.com/ubuntu/ precise-backports universe main multiverse restricted  
+deb-src http://mirrors.163.com/ubuntu/ precise-updates universe main multiverse restricted 
